8496604a4626fbaa8b65e3f8cd477b46d0fea117
Sujeet Sahani <sujeetsahani874@gmail.com>
2026-07-23 21:07:42 +0530

    Day 5 - Java Pattern Programs

---
+++ src/day05_patterns/Square.java
@@ -0,0 +1,15 @@
+package day05_patterns;
+
+public class Square {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 7; i++) {
+
+            for (int j = 1; j <= 5; j++) {
+                System.out.print( "* ");
+            }
+
+            System.out.println();
+        }
+    }
+}
