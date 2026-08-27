5a8273acc0bbdf9837152159ff9de9f1c4c881c9
Sujeet Sahani <sujeetsahani874@gmail.com>
2026-08-27 23:57:39 +0530

    Day 16 Collections Practice

--- src/day16_collections_utility/FrequecyCount.java
+++ src/day16_collections_utility/FrequecyCount.java
@@ -5,13 +5,14 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.LinkedHashSet;
 
-public class UniqueFrequency {
+public class FrequecyCount {
 
     public static void main(String[] args) {
 
-        ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(10, 20, 10, 30, 20, 40));
+        ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(10,30,10,40,50, 20, 10, 30, 20, 40));
 
         LinkedHashSet<Integer> uniqueNumbers = new LinkedHashSet<>(numbers);
+        System.out.println("All numbers"+numbers);
 
         for (Integer number : uniqueNumbers) {
 
@@ -22,4 +23,5 @@ public class UniqueFrequency {
 
         }
     }
+
 }
